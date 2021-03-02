a190417ab2aba201e0b70712f6bfcb591bd1a1ed
Ismael Rojas <ismael711@gmail.com>
2021-03-01 21:23:49 -0300

    Final da aula 4

--- src/EntendendoLoopWhile.java
+++ src/EntendendoLoopWhile.java
@@ -1,17 +1,21 @@
 import java.util.Scanner;
 
-public class LoopWhile {
+public class EntendendoLoopWhile {
 
 	public static void main(String[] args) {
 		/*Imagine que uma ONG precise saber a média de idades dos alunos de uma escola.
-		 * Sabendo de antemão que exisntem 5 alunos e sem a necessidade de armazenar permanentemente a idade de cada um desses alunos, qual é a melhor forma de calcular essa média?*/
+		 * Sabendo de antemão quantos alunos existem e sem a necessidade de armazenar permanentemente a idade de cada um desses alunos, qual é a melhor forma de calcular essa média?*/
 		int idade;
 		double media=0;
-		int quantidadeAlunos = 0;
+		int quantidadeAlunos=0;
+		int totalAlunos;
 		
 		Scanner leitor = new Scanner(System.in);
 		
-		while(quantidadeAlunos < 5) {
+		System.out.println("Informe a quantidade de Alunos: ");
+		totalAlunos = leitor.nextInt();
+		
+		while(quantidadeAlunos < totalAlunos) {
 			System.out.println("Informe a idade do " + (quantidadeAlunos + 1) + "ºaluno");
 			idade = leitor.nextInt();
 			media = media + idade;
@@ -20,12 +24,11 @@ public class LoopWhile {
 			//quantidadeAlunos ++;
 		}
 		
-		media = media/5.0;
+		media = media/(double) totalAlunos;
 		
 		System.out.println("A média das idades é " + media);
 		
 		leitor.close();
-		
 	}
 
 }
